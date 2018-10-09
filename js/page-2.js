2c987544688ba35e20a11acef3f8cb0a06250621
bitqueen <lisnaebit@gmail.com>
2018-10-09 17:01:42 +0700

    styling

--- js/page-2.js
+++ js/page-2.js
@@ -1,69 +1,101 @@
-
-var chart = Highcharts.chart('content-1-chart', {
-
+Highcharts.chart('content-1-chart', {
+    chart: {
+        type: 'column'
+    },
     title: {
-        text: 'Chart.update'
+        text: ' '
     },
-
-    subtitle: {
-        text: 'Plain'
-    },
-
+    credits: {
+      enabled: false
+  },
+    
     xAxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        categories: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+        ],
+        crosshair: true
     },
-
+    yAxis: {
+        min: 0,
+        title: {
+            text: ' '
+        }
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        }
+    },
     series: [{
-        type: 'column',
-        colorByPoint: true,
-        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
-        showInLegend: false
+        data: [50, 90, 80, 40, 120, 100, 80, 140]
+
     }]
 });
-$('#plain').click(function () {
-    chart.update({
-        chart: {
-            inverted: false,
-            polar: false
-        },
-        subtitle: {
-            text: 'Plain'
-        }
-    });
-});
-var chart = Highcharts.chart('content-2-chart', {
-
+Highcharts.chart('content-2-chart', {
+    chart: {
+        type: 'column'
+    },
     title: {
-        text: 'Chart.update'
+        text: ' '
     },
-
-    subtitle: {
-        text: 'Plain'
-    },
-
+    credits: {
+      enabled: false
+  },
+    
     xAxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        categories: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+        ],
+        crosshair: true
     },
-
+    yAxis: {
+        min: 0,
+        title: {
+            text: ' '
+        }
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        }
+    },
     series: [{
-        type: 'column',
-        colorByPoint: true,
-        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
-        showInLegend: false
+        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5]
+
     }]
 });
-$('#plain').click(function () {
-    chart.update({
-        chart: {
-            inverted: false,
-            polar: false
-        },
-        subtitle: {
-            text: 'Plain'
-        }
-    });
-});
-
 Highcharts.chart('content-3-chart', {
     chart: {
         type: 'column'
