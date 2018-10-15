3e62e2648dcf0b2e04109841880841daae2619fc
bitqueen <lisnaebit@gmail.com>
2018-10-15 16:26:43 +0700

    styling

--- js/page-12.js
+++ js/page-12.js
@@ -58,187 +58,202 @@ Highcharts.chart('content3', {
     }]
 });
 Highcharts.chart('content4', {
-
+    chart: {
+        type: 'areaspline'
+    },
     title: {
-        text: ''
+        text: ' '
     },
-
+    legend: {
+        layout: 'vertical',
+        align: 'left',
+        verticalAlign: 'top',
+        x: 150,
+        y: 100,
+        floating: true,
+        borderWidth: 1,
+        backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+    },
     xAxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']
+        categories: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+        ],
+        plotBands: [{ // visualize the weekend
+            from: 4.5,
+            to: 6.5,
+            color: 'rgba(255,133,51, .2)'
+        }]
     },
-    credits: {
-          enabled: false
-      },
-    defs: {
-        gradient0: {
-            tagName: 'linearGradient',
-            id: 'gradient-0',
-            x1: 0,
-            y1: 0,
-            x2: 0,
-            y2: 1,
-            children: [{
-                tagName: 'stop',
-                offset: 0
-            }, {
-                tagName: 'stop',
-                offset: 1
-            }]
-        },
-
-        gradient1: {
-            tagName: 'linearGradient',
-            id: 'gradient-1',
-            x1: 0,
-            y1: 0,
-            x2: 0,
-            y2: 1,
-            children: [{
-                tagName: 'stop',
-                offset: 0
-            }, {
-                tagName: 'stop',
-                offset: 1
-            }]
+    yAxis: {
+        title: {
+            text: ' '
         }
     },
-
+    tooltip: {
+        shared: true,
+        valueSuffix: ' units'
+    },
+    credits: {
+        enabled: false
+    },
+    plotOptions: {
+        areaspline: {
+            fillOpacity: 0.5
+        }
+    },
     series: [{
-        type: 'area',
-        keys: ['y', 'selected'],
-        data: [
-            [29.9, false],
-            [71.5, false],
-            [55.4, false],
-            [129.2, false],
-            [54.0, false],
-            [17.0, false],
-        ]
+        name: 'candidate-1',
+        color: 'rgba(255,133,51,0.2)',
+        data: [3, 4, 3, 5, 4, 10]
+    }, {
+        name: 'candidate-2',
+        color: 'rgba(255,204,0,0.2)',
+        data: [1, 3, 4, 3, 3, 5]
     }]
 });
 Highcharts.chart('job-chart3-4', {
-  title: {
-        text: ''
+    chart: {
+        type: 'column'
     },
-
-  chart: {
-    type: 'variwide'
-  },
-
-
-  xAxis: {
-    type: 'category',
-
-  },
-  credits: {
+    title: {
+        text: ' '
+    },
+    credits: {
       enabled: false
   },
-  legend: {
-    enabled: false
-  },
-
-  series: [{
-    data: [
-      ['1', 20, 300000],
-      ['2', 30, 300000],
-      ['3', 20, 300000],
-      ['4', 30, 300000],
-      ['5', 40, 300000],
-      ['6', 20, 300000],
-      ['7', 25, 300000],
-      ['8', 30, 300000],
-      ['9', 25, 300000],
-      ['10', 10, 300000],
-      ['11', 20, 300000],
-      ['12', 30, 300000],
-      ['13', 20, 300000],
-
-
-    ],
-
-  }]
-});
-Highcharts.chart('job-chart3-2', {
-  title: {
-        text: ''
+    
+    xAxis: {
+        categories: [
+            'Black Campaign',
+            'Perusak Atribut',
+            'Ujaran Kebencian',
+            'Hoax',
+            'Politik Uang',
+            'Lain Lain',
+        ],
+        crosshair: true
     },
-
-  chart: {
-    type: 'variwide'
-  },
-
-
-  xAxis: {
-    type: 'category',
-
-  },
-  credits: {
-      enabled: false
-  },
-  legend: {
-    enabled: false
-  },
-
-  series: [{
-    data: [
-      ['1', 20, 300000],
-      ['2', 30, 300000],
-      ['3', 20, 300000],
-      ['4', 30, 300000],
-      ['5', 40, 300000],
-      ['6', 20, 300000],
-      ['7', 25, 300000],
-      ['8', 30, 300000],
-      ['9', 25, 300000],
-      ['10', 10, 300000],
-      ['11', 20, 300000],
-      ['12', 30, 300000],
-      ['13', 20, 300000],
-
-
-    ],
-
-  }]
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Score'
+        }
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        }
+    },
+    series: [{
+        data: [50, 90, 80, 40, 120],
+        colorByPoint: true
+    }]
 });
 Highcharts.chart('job-chart3-3', {
-  title: {
-        text: ''
+    chart: {
+        type: 'column'
     },
-
-  chart: {
-    type: 'variwide'
-  },
-
-
-  xAxis: {
-    type: 'category',
-
-  },
-  credits: {
+    title: {
+        text: ' '
+    },
+    credits: {
       enabled: false
   },
-  legend: {
-    enabled: false
+    
+    xAxis: {
+        categories: [
+            'Black Campaign',
+            'Perusak Atribut',
+            'Ujaran Kebencian',
+            'Hoax',
+            'Politik Uang',
+            'Lain Lain',
+        ],
+        crosshair: true
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Score'
+        }
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        }
+    },
+    series: [{
+        data: [50, 90, 80, 40, 120],
+        colorByPoint: true
+    }]
+});
+Highcharts.chart('job-chart3-2', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: ' '
+    },
+    credits: {
+      enabled: false
   },
-
-  series: [{
-    data: [
-      ['1', 20, 300000],
-      ['2', 30, 300000],
-      ['3', 20, 300000],
-      ['4', 30, 300000],
-      ['5', 40, 300000],
-      ['6', 20, 300000],
-      ['7', 25, 300000],
-      ['8', 30, 300000],
-      ['9', 25, 300000],
-      ['10', 10, 300000],
-      ['11', 20, 300000],
-      ['12', 30, 300000],
-      ['13', 20, 300000],
-
-
-    ],
-
-  }]
+    
+    xAxis: {
+        categories: [
+            'Black Campaign',
+            'Perusak Atribut',
+            'Ujaran Kebencian',
+            'Hoax',
+            'Politik Uang',
+            'Lain Lain',
+        ],
+        crosshair: true
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Score'
+        }
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        }
+    },
+    series: [{
+        data: [50, 90, 80, 40, 120],
+        colorByPoint: true
+    }]
 });
