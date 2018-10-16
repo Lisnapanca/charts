565e27e2cee09c76cf5096cbd7327fc90773edb6
bitqueen <lisnaebit@gmail.com>
2018-10-16 13:42:05 +0700

    styling

--- js/page-14.js
+++ js/page-14.js
@@ -125,61 +125,61 @@ Highcharts.chart('job-chart3-3', {
   }]
 });
 Highcharts.chart('content3', {
-
+    chart: {
+        type: 'areaspline'
+    },
     title: {
-        text: ''
+        text: ' '
     },
-
+    legend: {
+        layout: 'vertical',
+        align: 'left',
+        verticalAlign: 'top',
+        x: 150,
+        y: 100,
+        floating: true,
+        borderWidth: 1,
+        backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+    },
     xAxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']
+        categories: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+        ],
+        plotBands: [{ // visualize the weekend
+            from: 4.5,
+            to: 6.5,
+            color: 'rgba(255,133,51, .2)'
+        }]
     },
-    credits: {
-          enabled: false
-      },
-    defs: {
-        gradient0: {
-            tagName: 'linearGradient',
-            id: 'gradient-0',
-            x1: 0,
-            y1: 0,
-            x2: 0,
-            y2: 1,
-            children: [{
-                tagName: 'stop',
-                offset: 0
-            }, {
-                tagName: 'stop',
-                offset: 1
-            }]
-        },
-
-        gradient1: {
-            tagName: 'linearGradient',
-            id: 'gradient-1',
-            x1: 0,
-            y1: 0,
-            x2: 0,
-            y2: 1,
-            children: [{
-                tagName: 'stop',
-                offset: 0
-            }, {
-                tagName: 'stop',
-                offset: 1
-            }]
+    yAxis: {
+        title: {
+            text: ' '
         }
     },
-
+    tooltip: {
+        shared: true,
+        valueSuffix: ' units'
+    },
+    credits: {
+        enabled: false
+    },
+    plotOptions: {
+        areaspline: {
+            fillOpacity: 0.5
+        }
+    },
     series: [{
-        type: 'area',
-        keys: ['y', 'selected'],
-        data: [
-            [29.9, false],
-            [71.5, false],
-            [55.4, false],
-            [129.2, false],
-            [54.0, false],
-            [17.0, false],
-        ]
+        name: 'candidate-1',
+        color: 'rgba(255,133,51,0.2)',
+        data: [3, 4, 3, 5, 4, 10]
+    }, {
+        name: 'candidate-2',
+        color: 'rgba(255,204,0,0.2)',
+        data: [1, 3, 4, 3, 3, 5]
     }]
 });
