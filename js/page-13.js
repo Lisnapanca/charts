15f63500ef396c9157382e7fc270dd7650c1f515
bitqueen <lisnaebit@gmail.com>
2018-10-16 10:59:45 +0700

    styling

--- js/page-13.js
+++ js/page-13.js
@@ -124,188 +124,188 @@ Highcharts.chart('job-chart3-3', {
 
   }]
 });
-// Highcharts.chart('job-chart3-4', {
-//   title: {
-//         text: ''
-//     },
-
-//   chart: {
-//     type: 'variwide'
-//   },
-
-
-//   xAxis: {
-//     type: 'category',
-
-//   },
-//   credits: {
-//       enabled: false
-//   },
-//   legend: {
-//     enabled: false
-//   },
-
-//   series: [{
-//     data: [
-//       ['1', 20, 300000],
-//       ['2', 30, 300000],
-//       ['3', 20, 300000],
-//       ['4', 30, 300000],
-//       ['5', 40, 300000],
-//       ['6', 20, 300000],
-//       ['7', 25, 300000],
-//       ['8', 30, 300000],
-//       ['9', 25, 300000],
-//       ['10', 10, 300000],
-//       ['11', 20, 300000],
-//       ['12', 30, 300000],
-//       ['13', 20, 300000],
-
-
-//     ],
-
-//   }]
-// });
-// Highcharts.chart('job-chart3-5', {
-//   title: {
-//         text: ''
-//     },
-
-//   chart: {
-//     type: 'variwide'
-//   },
-
-
-//   xAxis: {
-//     type: 'category',
-
-//   },
-//   credits: {
-//       enabled: false
-//   },
-//   legend: {
-//     enabled: false
-//   },
-
-//   series: [{
-//     data: [
-//       ['1', 20, 300000],
-//       ['2', 30, 300000],
-//       ['3', 20, 300000],
-//       ['4', 30, 300000],
-//       ['5', 40, 300000],
-//       ['6', 20, 300000],
-//       ['7', 25, 300000],
-//       ['8', 30, 300000],
-//       ['9', 25, 300000],
-//       ['10', 10, 300000],
-//       ['11', 20, 300000],
-//       ['12', 30, 300000],
-//       ['13', 20, 300000],
-
-
-//     ],
-
-//   }]
-// });
-// Highcharts.chart('job-chart3-6', {
-//   title: {
-//         text: ''
-//     },
-
-//   chart: {
-//     type: 'variwide'
-//   },
-
-
-//   xAxis: {
-//     type: 'category',
-
-//   },
-//   credits: {
-//       enabled: false
-//   },
-//   legend: {
-//     enabled: false
-//   },
-
-//   series: [{
-//     data: [
-//       ['1', 20, 300000],
-//       ['2', 30, 300000],
-//       ['3', 20, 300000],
-//       ['4', 30, 300000],
-//       ['5', 40, 300000],
-//       ['6', 20, 300000],
-//       ['7', 25, 300000],
-//       ['8', 30, 300000],
-//       ['9', 25, 300000],
-//       ['10', 10, 300000],
-//       ['11', 20, 300000],
-//       ['12', 30, 300000],
-//       ['13', 20, 300000],
-
-
-//     ],
-
-//   }]
-// });
-Highcharts.chart('content3', {
-
-    title: {
+Highcharts.chart('job-chart3-4', {
+  title: {
         text: ''
     },
 
-    xAxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']
-    },
-    credits: {
-          enabled: false
-      },
-    defs: {
-        gradient0: {
-            tagName: 'linearGradient',
-            id: 'gradient-0',
-            x1: 0,
-            y1: 0,
-            x2: 0,
-            y2: 1,
-            children: [{
-                tagName: 'stop',
-                offset: 0
-            }, {
-                tagName: 'stop',
-                offset: 1
-            }]
-        },
+  chart: {
+    type: 'variwide'
+  },
 
-        gradient1: {
-            tagName: 'linearGradient',
-            id: 'gradient-1',
-            x1: 0,
-            y1: 0,
-            x2: 0,
-            y2: 1,
-            children: [{
-                tagName: 'stop',
-                offset: 0
-            }, {
-                tagName: 'stop',
-                offset: 1
-            }]
+
+  xAxis: {
+    type: 'category',
+
+  },
+  credits: {
+      enabled: false
+  },
+  legend: {
+    enabled: false
+  },
+
+  series: [{
+    data: [
+      ['1', 20, 300000],
+      ['2', 30, 300000],
+      ['3', 20, 300000],
+      ['4', 30, 300000],
+      ['5', 40, 300000],
+      ['6', 20, 300000],
+      ['7', 25, 300000],
+      ['8', 30, 300000],
+      ['9', 25, 300000],
+      ['10', 10, 300000],
+      ['11', 20, 300000],
+      ['12', 30, 300000],
+      ['13', 20, 300000],
+
+
+    ],
+
+  }]
+});
+Highcharts.chart('job-chart3-5', {
+  title: {
+        text: ''
+    },
+
+  chart: {
+    type: 'variwide'
+  },
+
+
+  xAxis: {
+    type: 'category',
+
+  },
+  credits: {
+      enabled: false
+  },
+  legend: {
+    enabled: false
+  },
+
+  series: [{
+    data: [
+      ['1', 20, 300000],
+      ['2', 30, 300000],
+      ['3', 20, 300000],
+      ['4', 30, 300000],
+      ['5', 40, 300000],
+      ['6', 20, 300000],
+      ['7', 25, 300000],
+      ['8', 30, 300000],
+      ['9', 25, 300000],
+      ['10', 10, 300000],
+      ['11', 20, 300000],
+      ['12', 30, 300000],
+      ['13', 20, 300000],
+
+
+    ],
+
+  }]
+});
+Highcharts.chart('job-chart3-6', {
+  title: {
+        text: ''
+    },
+
+  chart: {
+    type: 'variwide'
+  },
+
+
+  xAxis: {
+    type: 'category',
+
+  },
+  credits: {
+      enabled: false
+  },
+  legend: {
+    enabled: false
+  },
+
+  series: [{
+    data: [
+      ['1', 20, 300000],
+      ['2', 30, 300000],
+      ['3', 20, 300000],
+      ['4', 30, 300000],
+      ['5', 40, 300000],
+      ['6', 20, 300000],
+      ['7', 25, 300000],
+      ['8', 30, 300000],
+      ['9', 25, 300000],
+      ['10', 10, 300000],
+      ['11', 20, 300000],
+      ['12', 30, 300000],
+      ['13', 20, 300000],
+
+
+    ],
+
+  }]
+});
+Highcharts.chart('content3', {
+    chart: {
+        type: 'areaspline'
+    },
+    title: {
+        text: ' '
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'left',
+        verticalAlign: 'top',
+        x: 150,
+        y: 100,
+        floating: true,
+        borderWidth: 1,
+        backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+    },
+    xAxis: {
+        categories: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+        ],
+        plotBands: [{ // visualize the weekend
+            from: 4.5,
+            to: 6.5,
+            color: 'rgba(255,133,51, .2)'
+        }]
+    },
+    yAxis: {
+        title: {
+            text: ' '
         }
     },
-
+    tooltip: {
+        shared: true,
+        valueSuffix: ' units'
+    },
+    credits: {
+        enabled: false
+    },
+    plotOptions: {
+        areaspline: {
+            fillOpacity: 0.5
+        }
+    },
     series: [{
-        type: 'area',
-        keys: ['y', 'selected'],
-        data: [
-            [29.9, false],
-            [71.5, false],
-            [55.4, false],
-            [129.2, false],
-            [54.0, false],
-            [17.0, false],
-        ]
+        name: 'candidate-1',
+        color: 'rgba(255,133,51,0.2)',
+        data: [3, 4, 3, 5, 4, 10]
+    }, {
+        name: 'candidate-2',
+        color: 'rgba(255,204,0,0.2)',
+        data: [1, 3, 4, 3, 3, 5]
     }]
 });
