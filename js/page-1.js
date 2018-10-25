fb24fd8f3858a899e468a3f100f696d29e32425b
bitqueen <lisnaebit@gmail.com>
2018-10-25 14:36:12 +0700

    styling

--- js/page-1.js
+++ js/page-1.js
@@ -1,4 +1,5 @@
-Highcharts.chart('content2-chart1', {
+function bar_chart(selected_id){
+    Highcharts.chart(selected_id, {
   title: {
         text: ''
     },
@@ -40,218 +41,16 @@ Highcharts.chart('content2-chart1', {
 
   }]
 });
-Highcharts.chart('content2-chart2', {
-  title: {
-        text: ''
-    },
-
+  };
+bar_chart('content2-chart1')
+bar_chart('content2-chart2')
+bar_chart('content2-chart3')
+bar_chart('content5-chart1')
+bar_chart('content5-chart2')
+bar_chart('content5-chart3')
+function spline_chart(selected_id){
+    Highcharts.chart(selected_id, {
   chart: {
-    type: 'variwide'
-  },
-
-
-  xAxis: {
-    type: 'category',
-
-  },
-  credits: {
-      enabled: false
-  },
-  legend: {
-    enabled: false
-  },
-
-  series: [{
-    data: [
-      ['1', 20, 300000],
-      ['2', 30, 300000],
-      ['3', 20, 300000],
-      ['4', 30, 300000],
-      ['5', 40, 300000],
-      ['6', 20, 300000],
-      ['7', 25, 300000],
-      ['8', 30, 300000],
-      ['9', 25, 300000],
-      ['10', 10, 300000],
-      ['11', 20, 300000],
-      ['12', 30, 300000],
-      ['13', 20, 300000],
-
-
-    ],
-
-  }]
-});
-Highcharts.chart('content2-chart3', {
-  title: {
-        text: ''
-    },
-
-  chart: {
-    type: 'variwide'
-  },
-
-
-  xAxis: {
-    type: 'category',
-
-  },
-  credits: {
-      enabled: false
-  },
-  legend: {
-    enabled: false
-  },
-
-  series: [{
-    data: [
-      ['1', 20, 300000],
-      ['2', 30, 300000],
-      ['3', 20, 300000],
-      ['4', 30, 300000],
-      ['5', 40, 300000],
-      ['6', 20, 300000],
-      ['7', 25, 300000],
-      ['8', 30, 300000],
-      ['9', 25, 300000],
-      ['10', 10, 300000],
-      ['11', 20, 300000],
-      ['12', 30, 300000],
-      ['13', 20, 300000],
-
-
-    ],
-
-  }]
-});
-Highcharts.chart('content5-chart1', {
-  title: {
-        text: ' '
-    },
-
-  chart: {
-    type: 'variwide'
-  },
-
-
-  xAxis: {
-    type: 'category',
-
-  },
-  credits: {
-      enabled: false
-  },
-  legend: {
-    enabled: false
-  },
-
-  series: [{
-    data: [
-      ['1', 20, 300000],
-      ['2', 30, 300000],
-      ['3', 20, 300000],
-      ['4', 30, 300000],
-      ['5', 40, 300000],
-      ['6', 20, 300000],
-      ['7', 25, 300000],
-      ['8', 30, 300000],
-      ['9', 25, 300000],
-      ['10', 10, 300000],
-      ['11', 20, 300000],
-      ['12', 30, 300000],
-      ['13', 20, 300000],
-
-
-    ],
-
-  }]
-});
-Highcharts.chart('content5-chart2', {
-  title: {
-        text: ''
-    },
-
-  chart: {
-    type: 'variwide'
-  },
-
-
-  xAxis: {
-    type: 'category',
-
-  },
-  credits: {
-      enabled: false
-  },
-  legend: {
-    enabled: false
-  },
-
-  series: [{
-    data: [
-      ['1', 20, 300000],
-      ['2', 30, 300000],
-      ['3', 20, 300000],
-      ['4', 30, 300000],
-      ['5', 40, 300000],
-      ['6', 20, 300000],
-      ['7', 25, 300000],
-      ['8', 30, 300000],
-      ['9', 25, 300000],
-      ['10', 10, 300000],
-      ['11', 20, 300000],
-      ['12', 30, 300000],
-      ['13', 20, 300000],
-
-
-    ],
-
-  }]
-});
-Highcharts.chart('content5-chart3', {
-  title: {
-        text: ''
-    },
-
-  chart: {
-    type: 'variwide'
-  },
-
-
-  xAxis: {
-    type: 'category',
-
-  },
-  credits: {
-      enabled: false
-  },
-  legend: {
-    enabled: false
-  },
-
-  series: [{
-    data: [
-      ['1', 20, 300000],
-      ['2', 30, 300000],
-      ['3', 20, 300000],
-      ['4', 30, 300000],
-      ['5', 40, 300000],
-      ['6', 20, 300000],
-      ['7', 25, 300000],
-      ['8', 30, 300000],
-      ['9', 25, 300000],
-      ['10', 10, 300000],
-      ['11', 20, 300000],
-      ['12', 30, 300000],
-      ['13', 20, 300000],
-
-
-    ],
-
-  }]
-});
-Highcharts.chart('content3-chart', {
-    chart: {
         type: 'areaspline'
     },
     title: {
@@ -309,65 +108,9 @@ Highcharts.chart('content3-chart', {
         data: [1, 3, 4, 3, 3, 5]
     }]
 });
-Highcharts.chart('content5-chart', {
-    chart: {
-        type: 'areaspline'
-    },
-    title: {
-        text: ' '
-    },
-    legend: {
-        layout: 'vertical',
-        align: 'left',
-        verticalAlign: 'top',
-        x: 150,
-        y: 100,
-        floating: true,
-        borderWidth: 1,
-        backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
-    },
-    xAxis: {
-        categories: [
-            '07:00 AM',
-            '08:00 AM',
-            '09:00 AM',
-            '10:00 AM',
-            '11:00 AM',
-            '12:00 AM',
-        ],
-        plotBands: [{ // visualize the weekend
-            from: 4.5,
-            to: 6.5,
-            color: 'rgba(255,133,51, .2)'
-        }]
-    },
-    yAxis: {
-        title: {
-            text: ' '
-        }
-    },
-    tooltip: {
-        shared: true,
-        valueSuffix: ' units'
-    },
-    credits: {
-        enabled: false
-    },
-    plotOptions: {
-        areaspline: {
-            fillOpacity: 0.5
-        }
-    },
-    series: [{
-        name: 'candidate-1',
-        color: 'rgba(255,133,51,0.2)',
-        data: [3, 4, 3, 5, 4, 10]
-    }, {
-        name: 'candidate-2',
-        color: 'rgba(255,204,0,0.2)',
-        data: [1, 3, 4, 3, 3, 5]
-    }]
-});
+  };
+spline_chart('content3-chart')
+spline_chart('content5-chart')
 $( document ).ready(function() {
   setTimeout(function(){
     $(".highcharts-plot-bands-0").attr('id', 'chart-content3');
--- js/page-2.js
+++ js/page-2.js
@@ -1,42 +1,5 @@
-// Highcharts.theme = {
-//     colors: ['#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', 
-//              '#FF9655', '#FFF263', '#6AF9C4'],
-//     chart: {
-//         backgroundColor: {
-//             linearGradient: [0, 0, 500, 500],
-//             stops: [
-//                 [0, 'rgb(255, 255, 255)'],
-//                 [1, 'rgb(240, 240, 255)']
-//             ]
-//         },
-//     },
-//     title: {
-//         style: {
-//             color: '#000',
-//             font: 'bold 16px "Trebuchet MS", Verdana, sans-serif'
-//         }
-//     },
-//     subtitle: {
-//         style: {
-//             color: '#666666',
-//             font: 'bold 12px "Trebuchet MS", Verdana, sans-serif'
-//         }
-//     },
-
-//     legend: {
-//         itemStyle: {
-//             font: '9pt Trebuchet MS, Verdana, sans-serif',
-//             color: 'black'
-//         },
-//         itemHoverStyle:{
-//             color: 'gray'
-//         }   
-//     }
-// };
-
-// Apply the theme
-// Highcharts.setOptions(Highcharts.theme);
-Highcharts.chart('content-1-chart', {
+function bar_chart(selected_id){
+    Highcharts.chart(selected_id, {
     chart: {
         type: 'column'
     },
@@ -85,106 +48,10 @@ Highcharts.chart('content-1-chart', {
         colorByPoint: true
     }]
 });
-Highcharts.chart('content-2-chart', {
-    chart: {
-        type: 'column'
-    },
-    title: {
-        text: ' '
-    },
-    credits: {
-      enabled: false
-  },
-    
-    xAxis: {
-        categories: [
-            'Nasionalis',
-            'Integritas',
-            'Bersih',
-            'Cerdas',
-            'Bijak',
-            'Jiwa Negarawan',
-            'Religius',
-            'Tegas',
-        ],
-        crosshair: true
-    },
-    yAxis: {
-        min: 0,
-        title: {
-            text: 'Score'
-        }
-    },
-    tooltip: {
-        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-        footerFormat: '</table>',
-        shared: true,
-        useHTML: true
-    },
-    plotOptions: {
-        column: {
-            pointPadding: 0.2,
-            borderWidth: 0
-        }
-    },
-    series: [{
-        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5],
-        colorByPoint: true
-
-    }]
-});
-Highcharts.chart('content-3-chart', {
-    chart: {
-        type: 'column'
-    },
-    title: {
-        text: ' '
-    },
-    credits: {
-      enabled: false
-  },
-    
-    xAxis: {
-        categories: [
-            'Nasionalis',
-            'Integritas',
-            'Bersih',
-            'Cerdas',
-            'Bijak',
-            'Jiwa Negarawan',
-            'Religius',
-            'Tegas',
-        ],
-        crosshair: true
-    },
-    yAxis: {
-        min: 0,
-        title: {
-            text: 'Score'
-        }
-    },
-    tooltip: {
-        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-        footerFormat: '</table>',
-        shared: true,
-        useHTML: true
-    },
-    plotOptions: {
-        column: {
-            pointPadding: 0.2,
-            borderWidth: 0
-        }
-    },
-    series: [{
-        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5],
-        colorByPoint: true
-
-    }]
-});
+};
+bar_chart('content-1-chart')
+bar_chart('content-2-chart')
+bar_chart('content-3-chart')
 var chart = AmCharts.makeChart( "content2-chart3", {
   "type": "pie",
   "credits": {
